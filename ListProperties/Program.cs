98148f19ac88c6e7169bc61ef9ebb42426100d03
pavani <pavaniavni456@gmail.com>
2022-02-09 12:20:40 +0530

    added listproperties

---
+++ ListProperties/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Collections.Generics
+{
+    internal class ListProperties
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Example of List");
+            List<int> list = new List<int>();
+            Console.WriteLine();
+            Console.WriteLine("Enter the values : ");
+            for (int i = 1; i < 5; i++)
+            {
+                list.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+            Console.WriteLine("Before Sorting of List");
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            //clear all data in the list
+            //list.Clear();
+            list.Sort();
+            Console.WriteLine("After Sorting of List");
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            //this will help us to find some value or search value in list
+            bool flag = list.Contains(9);
+            if (flag)
+            {
+                Console.WriteLine("We are able to find 9 in the list");
+            }
+            else
+            {
+                Console.WriteLine(" 9 was not there in the list");
+            }
+            list.Remove(1);
+            list.RemoveAt(1);
+            Console.WriteLine("After removal of items from List");
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+            
+
+        }
+    }
+}
